794947234a98a2cc232b887ceb77bf53c46f2e83
suryarajrs <suryars@outlook.in>
2024-02-14 01:38:35 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,46 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 
 const App = () => {
-  const [rating, setRating] = useState();
+  const [rating, setRating] = useState(5);
   const [comment, setComment] = useState();
-  const [commentError, setCommentError] = useState();
+  const [commentError, setCommentError] = useState(false);
 
+
+  const handleChange = (e)=>{
+    setRating(e.target.value);
+  }
+  const handleOnChange =(e)=>{
+   if (e.target.value.length < 5){
+    setCommentError(true);
+   }else{
+    setCommentError(false);
+  }
+}
   return (
     <div id="main">
       <form>
         <div>
           <label htmlFor='rating'>Rating: </label>
           <input 
+            onChange={handleChange}
             type="range"
             min="1"
             max="10"
             id="rating"
+            value={rating}  
           />
-          <span className='rating'>rating</span>
+          <span className='rating'>{rating}</span>
         </div>
+
         <div>
           <label htmlFor='comment'>Comment: </label>
           <textarea 
             id='comment'
+            onChange={handleOnChange}
           />
-          <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>
+          {commentError &&<p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>}
         </div>
-        <button type='submit'>Submit</button>
+        <button onClick={(e)=>{e.preventDefault()}} type='submit'>Submit</button>
       </form>
     </div>
   )
